d91ff4ddc5becde9d4be2de9da7fc31cdadf1153
Денис Васин <vasin@stabilis72.ru>
2020-10-30 14:36:05 +0500

    add comments

--- ExcelParser.py
+++ ExcelParser.py
@@ -2,31 +2,39 @@ from openpyxl import load_workbook
 
 
 def estimate_parser(file):
-    wb = load_workbook(file)
-    sheet = wb.active
-    data = []
-    results = ['Материалы', 'Машины и механизмы', 'ФОТ']
-    row_count = sheet.max_row
-    for row_number in range(1, row_count):
+    """
+    Принимает файл excel
+    парсит данные о сметах
+    добавляет все элементы документа в список
+    :param file:
+    :return: list
+    """
+    wb = load_workbook(file)  # Открываем файл таблицы
+    sheet = wb.active  # выбираем активный лист
+    data = []  # создаем список для данных сметы
+    results = ['Материалы', 'Машины и механизмы', 'ФОТ']  # создаем список для исключений
+    row_count = sheet.max_row  # находим количество строк
+    for row_number in range(1, row_count):  # итерация по каждой строке
+        # находим значение каждого столбца в строке
         cell_a = str(sheet[f'A{row_number}'].value)
         cell_b = str(sheet[f'B{row_number}'].value)
         cell_c = str(sheet[f'C{row_number}'].value)
         cell_d = str(sheet[f'D{row_number}'].value)
         cell_f = str(sheet[f'F{row_number}'].value)
         cell_g = str(sheet[f'G{row_number}'].value)
-        if "Раздел" in cell_a:
-            data.append(cell_a.strip())
-        elif sheet[f'B{row_number}'].value is None:
-            if sheet[f'A{row_number}'].value is not None and cell_a.strip() not in results and \
-                    "итог" not in cell_a.lower() and "ВСЕГО" not in cell_a:
-                data.append(cell_a.strip())
-        if 'СРК' in cell_b:
-            data.append([cell_b, cell_c])
-        if "Цена по прайсу" in cell_b:
-            data.append([cell_b, cell_c, cell_d, cell_f, cell_g])
-        if "Механизмы" in cell_b:
-            data.append([cell_b, cell_c, cell_d,cell_f, cell_g])
-    return data
+        if "Раздел" in cell_a:  # Если слово Раздел в ячейке А
+            data.append(cell_a.strip())  # добавляем в список
+        elif cell_b == 'None':  # если ячейка Б пустая
+            if cell_a != 'None' and cell_a.strip() not in results and \
+                    "итог" not in cell_a.lower() and "ВСЕГО" not in cell_a:  # и ячейка А не пуста и значение А не в списке исключений
+                data.append(cell_a.strip())  # добавляем в список
+        if 'СРК' in cell_b:  # Если СРК в ячейке Б
+            data.append([cell_b, cell_c])  # добавляем в список
+        if "Цена по прайсу" in cell_b:  # Если Цена по прайсу в ячейке С
+            data.append([cell_b, cell_c, cell_d, cell_f, cell_g])  # добавляем в список
+        if "Механизмы" in cell_b:  # Если Механизмы в ячейке Б
+            data.append([cell_b, cell_c, cell_d, cell_f, cell_g])  # добавляем в список
+    return data  # Возвращаем список
 
 
 section_ids = []
